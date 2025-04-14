439aa59f5fd4e16c21573f2e4ff7a056a1f2408c
Calvin <micheal.lim.ailen@gmail.com>
2025-04-14 16:12:01 +0800

    添加webp动画图标

---
+++ src/components/picList.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<p class="pic-list">
+  <span><img src="../assets/color game.webp" height="142" width="114" alt=""/></span>
+  <span><img src="../assets/jili_color_game.webp" height="142" width="114" alt=""/></span>
+  <span><img src="../assets/texas%20plus.webp" height="142" width="114" alt=""/></span>
+  <span><img src="../assets/mines.webp" height="142" width="114" alt=""/></span>
+</p>
+</template>
+
+<style scoped>
+.pic-list{ width: auto;}
+.pic-list span{ display:inline-block; margin: 10px}
+</style>
