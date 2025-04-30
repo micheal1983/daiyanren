fa1648b50197d837fc508e5fa6058f2520fb410e
Calvin <micheal.lim.ailen@gmail.com>
2025-04-30 18:53:09 +0800

    生成key

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base:'/daiyanren/',
     plugins: [vue()],
-    build: {
-        outDir: 'docs'
-    }
+    // build: {
+    //     outDir: 'docs'
+    // }
 })
