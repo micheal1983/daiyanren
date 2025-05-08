0024b5c7558e70b5796cb856066c7433a7a82a86
micheal1983 <105361495+micheal1983@users.noreply.github.com>
2025-05-08 14:48:22 +0800

    首页轮播banner，未实现

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,8 +7,15 @@
     <source src="../assets/video/banner-before-loop.mp4?aaa=111" type="video/mp4">
     您的浏览器不支持 video 属性。
   </video>
+<!--  <div id="banner">-->
+<!--    <img src="../../public/images/banner.webp" width="100%"/>-->
+<!--  </div>-->
+<!--  <div id="page"><img src="../assets/ms/page.png" width="100%"/></div>-->
 </template>
 
 <style scoped>
-
+/*#banner{ width: 362px; margin: 8px auto;}
+//#banner img{ border-radius: 15px;}
+//#page{ width: 58px; height: 4px; margin: 0 auto; text-align: center; background: #646cff; font-size: 0;}
+//#page img{ margin: 0;}*/
 </style>
