e069f46f3497bf87532ec7b1b8ed7bdabdab4a94
calvin <micheal.lim.ailen@gmail.com>
2025-08-13 17:51:38 +0800

    color game

--- src/views/game.vue
+++ src/views/game.vue
@@ -4,26 +4,31 @@ import SpinePixi from "../components/SpinePixi.vue";
 </script>
 
 <template>
-  <video
-      ref="videoPlayer"
-      src="/video/game.mp4"
-      autoplay
-      muted
-      loop
-      playsinline
-      class="video-fullscreen"
-      width="100%"
-  ></video>
-  <SpinePixi></SpinePixi>
+  <div class="video-fullscreen">
+    <SpinePixi></SpinePixi>
+    <div class="gamepic">
+      <img src="/images/color%20game.webp" width="100%" />
+    </div>
+    <video
+        ref="videoPlayer"
+        src="/video/game2.mp4"
+        autoplay
+        muted
+        loop
+        playsinline
+        width="100%"
+    />
+  </div>
+
+
 </template>
 
 <style scoped>
 .video-fullscreen {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  object-fit: cover;
+  position: relative;
+  width: 390px;
+  object-fit: contain;
+  z-index: 1;
 }
+.gamepic{ width: 72px; height: 80px; position: fixed; right: 2px; margin-top: 260px; z-index: 999;}
 </style>
